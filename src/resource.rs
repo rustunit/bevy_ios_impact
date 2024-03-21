b98e36d1364b8366b98d1e21119c7348b5fb9362
extrawurst <mail@rusticorn.com>
2024-03-21 12:27:38 -0700

    allow disabling impacts on API level

--- src/resource.rs
+++ src/resource.rs
@@ -9,9 +9,31 @@ use crate::FeedbackStyle;
 #[derive(Resource, Clone, Debug, Default)]
 pub struct ImpactResource {
     inner: SharedGenerators,
+    disabled: bool,
 }
 
 impl ImpactResource {
+    /// allows disabling impacts without checking in user-code whether or not to call the `impact` APIs
+    pub fn disable(&mut self) {
+        self.disabled = true;
+    }
+
+    /// enable impacts
+    pub fn enable(&mut self) {
+        self.disabled = true;
+    }
+
+    /// allows fetching enabled state
+    pub fn enabled(&mut self) -> bool {
+        self.disabled
+    }
+
+    /// toggles enabled state and returns state it toggled to
+    pub fn toggle(&mut self) -> bool {
+        self.disabled = !self.disabled;
+        self.disabled
+    }
+
     /// Prepares the Taptic engine.
     /// According to apple docs the engine might be asleep if not prepared, which could leads to delays.
     /// If not used via eg. [impact] the engine will go back to sleep to save battery.
@@ -21,7 +43,9 @@ impl ImpactResource {
     ///
     /// on platforms other than ios this becomes a no-op.
     pub fn prepare(&mut self) {
-        self.inner.prepare();
+        if !self.disabled {
+            self.inner.prepare();
+        }
     }
 
     /// Triggers an impact with the given `style`.
@@ -31,7 +55,9 @@ impl ImpactResource {
     ///
     /// on platforms other than ios this becomes a no-op.
     pub fn impact(&mut self, style: FeedbackStyle) {
-        self.inner.impact(style);
+        if !self.disabled {
+            self.inner.impact(style);
+        }
     }
 
     /// Triggers an impact with the given `style` and `intensity`
@@ -41,6 +67,8 @@ impl ImpactResource {
     ///
     /// on platforms other than ios this becomes a no-op.
     pub fn impact_with_intensity(&mut self, style: FeedbackStyle, intensity: f64) {
-        self.inner.impact_with_intensity(style, intensity);
+        if !self.disabled {
+            self.inner.impact_with_intensity(style, intensity);
+        }
     }
 }
